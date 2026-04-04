e7f8f81f4c0f45c0ec462230c831d21d03568de3
Dr05 <87918036+Dr05s@users.noreply.github.com>
2026-04-04 08:33:31 +0700

    fs: proc: task_mmu: completed susfs patch

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -30,6 +30,10 @@
 #include <asm/tlbflush.h>
 #include "internal.h"
 
+#ifdef CONFIG_KSU_SUSFS
+#include <linux/susfs.h>
+#endif
+
 #ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
 extern void susfs_sus_ino_for_show_map_vma(unsigned long ino, dev_t *out_dev, unsigned long *out_ino);
 #endif
@@ -506,6 +510,10 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma)
 	unsigned long start, end;
 	dev_t dev = 0;
 	const char *name = NULL;
+#ifdef CONFIG_KSU_SUSFS_SUS_MAP
+  char *out_name;
+  int ret = 0;
+#endif
 
 	if (file) {
 		struct inode *inode = file_inode(vma->vm_file);
@@ -545,6 +553,27 @@ bypass_orig_flow:
 
 	start = vma->vm_start;
 	end = vma->vm_end;
+	
+#ifdef CONFIG_KSU_SUSFS_SUS_MAP
+  out_name = kmalloc(SUSFS_MAX_LEN_PATHNAME, GFP_KERNEL);
+  if (!out_name)
+    goto orig_flow;
+  ret = susfs_sus_maps(ino, end - start, &ino, &dev, &flags, &pgoff, vma, out_name);
+
+orig_flow:
+#endif
+
+#ifdef CONFIG_KSU_SUSFS_SUS_MAP
+  if (ret == 2) {
+    seq_pad(m, ' ');
+    seq_puts(m, out_name);
+    seq_putc(m, '\n');
+    kfree(out_name);
+    return;
+  }
+  kfree(out_name);
+#endif
+
 	if (show_vma_header_prefix(m, start, end, flags, pgoff, dev, ino))
 		return;
 
